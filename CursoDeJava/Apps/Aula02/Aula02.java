6c4bec90364f1601ded447928db127b04a744df3
Varnahal <danielmarcelino91@gmail.com>
2023-04-25 13:39:53 -0300

    variaveis

---
+++ CursoDeJava/Apps/Aula02/Aula02.java
@@ -0,0 +1,9 @@
+package CursoDeJava.Apps.Aula02;
+
+public class Aula02 {
+    public static void main(String[] args) {
+        int num = 100;
+        System.out.printf("O valor da váriavel num, é: %d %n",num);
+        System.out.printf("%s%n", "Olá Mundo");
+    }
+}
