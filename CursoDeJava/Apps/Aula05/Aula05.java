4184ef32ab916e24343eb3c4a9f147c69d90a91d
Varnahal <danielmarcelino91@gmail.com>
2023-04-27 14:03:25 -0300

    loops em jaaj

---
+++ CursoDeJava/Apps/Aula05/Aula05.java
@@ -0,0 +1,24 @@
+package CursoDeJava.Apps.Aula05;
+
+public class Aula05 {
+    public static void main(String[] args){
+        for(int i=0;i<=10;i++){
+            System.out.printf("%s%d%n","Olá",i);
+        }
+
+        int i = 0;
+        while(i<=10){
+            System.out.printf("%s%d%n","Olá",i);
+            i++;
+        }
+        int j = 0;
+        do{
+            System.out.printf("%s%d%n","Olá",j);
+            j++;
+        }while(j<=10);
+
+
+
+    }
+    
+}
