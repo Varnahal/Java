d05eafb5613f4613e9aced15e2e41861561a02f8
Varnahal <danielmarcelino91@gmail.com>
2023-05-15 13:30:02 -0300

    super() em jaaj

---
+++ CursoDeJava/Apps/Aula13/Aula13.java
@@ -0,0 +1,8 @@
+package CursoDeJava.Apps.Aula13;
+
+public class Aula13 {
+    public static void main(String[] args) {
+        Aviao v1 = new Aviao("Pedro", 2, "caça");
+        v1.info();
+    }
+}
---
+++ CursoDeJava/Apps/Aula13/Veiculo.java
@@ -0,0 +1,16 @@
+package CursoDeJava.Apps.Aula13;
+
+public class Veiculo {
+    private String nome = "";
+    private int tipo=0;
+
+    public Veiculo(String nome, int tipo){
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+
+    public void info(){
+        System.out.printf("Nome: %s%n", this.nome);
+        System.out.printf("tipo: %s%n", this.tipo);
+    }
+}
